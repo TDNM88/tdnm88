770ab831accfe717cf288a74a2b3c55d57b329cf
TDNM88 <167914317+TDNM88@users.noreply.github.com>
2024-05-03 23:53:51 +0700

    Update crypto.js

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,14 +1,6 @@
-   import crypto from 'crypto';
+﻿// src/utils/crypto.js
+import crypto from 'crypto';
 
-   export const generateSignature = (method, url, appId, data, apiKey) => {
-     // Concatenate your data into a string as required by the API
-     const dataString = `${method}${url}${JSON.stringify(data)}${appId}`;
-
-     // Create a hash using the API key
-     const hash = crypto
-       .createHmac('sha256', apiKey)
-       .update(dataString)
-       .digest('hex');
-
-     return hash;
-   };
+export const createMD5 = (input) => {
+  return crypto.createHash('md5').update(input).digest('hex');
+};
