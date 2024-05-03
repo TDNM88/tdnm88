d25197ee738bd53dafbad8b77cf8fb26a443c6ba
TDNM88 <167914317+TDNM88@users.noreply.github.com>
2024-05-04 00:00:42 +0700

    Update crypto.js

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,6 +1,6 @@
 ﻿// src/utils/crypto.js
 import crypto from 'crypto';
 
-export const createMD5 = (input) => {
+export const createMD5 = (data) => {
   return crypto.createHash('md5').update(input).digest('hex');
 };
