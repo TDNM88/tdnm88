44e94eff2795f0b6e83aae5489a38cd17b00dafa
TDNM88 <167914317+TDNM88@users.noreply.github.com>
2024-05-03 23:23:13 +0700

    Update crypto.js

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,18 +1,14 @@
-// src/utils/crypto.js
-import crypto from 'crypto';
+   import crypto from 'crypto';
 
-// Function to generate a signature for API requests
-export const generateSignature = (method, url, appId, data, apiKey) => {
-  // Concatenate your data into a string as required by the API
-  const dataString = `${method}${url}${JSON.stringify(data)}${appId}`;
+   export const generateSignature = (method, url, appId, data, apiKey) => {
+     // Concatenate your data into a string as required by the API
+     const dataString = `${method}${url}${JSON.stringify(data)}${appId}`;
 
-  // Create a hash using the API key
-  const hash = crypto
-    .createHmac('sha256', apiKey)
-    .update(dataString)
-    .digest('hex');
+     // Create a hash using the API key
+     const hash = crypto
+       .createHmac('sha256', apiKey)
+       .update(dataString)
+       .digest('hex');
 
-  return hash;
-};
-
-// Other cryptographic functions can also be included
+     return hash;
+   };
